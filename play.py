c7bce8282883023df45c74f4281cb7f3ce923895
Martin Schultz Kristensen <martinsk@Martins-MacBook-Pro.local>
2018-03-25 18:39:12 +0200

    add omxplayer --loop

--- play.py
+++ play.py
@@ -4,10 +4,20 @@ import omxplayer
 from pathlib import Path
 from time import sleep
 
-VIDEO_PATH = Path("/home/pi/Desktop/elephant.mp4", args['-o', 'local'])
+VIDEO_PATH = Path("/home/pi/Desktop/elephant.mp4", args['-o', 'local', '--loop'])
 
 player = omxplayer.player.OMXPlayer(VIDEO_PATH)
 
 sleep(5)
+
+
+
+
+# while True:
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             movie.stop()
+#             playing = False
+
 
 player.quit()
