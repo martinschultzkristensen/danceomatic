250ff71b7349469b461979250e7edd84e784e48c
Martin Schultz Kristensen <martinsk@Martins-MacBook-Pro.local>
2018-04-08 19:17:15 +0200

    should work now with small vidio and terminate by button

--- whileloopetest.py
+++ whileloopetest.py
@@ -6,33 +6,21 @@ from pathlib import Path
 
 GPIO.setmode (GPIO.BOARD)
 GPIO.setwarnings (False)
-
-
 button1=12  # Descriptive Variable for pin 12
-GPIO.setup(button1,GPIO.IN,pull_up_down=GPIO.PUD_UP) # Set button1 as input and Activate pull up resisor
+GPIO.setup(button1,GPIO.IN,pull_up_down=GPIO.PUD_UP) # Set button1 as input and Activate pull up resistor
 
+vida = "/home/pi/Desktop/elephant.mp4"
 
-while(1): # Create an infinite loop
-    player = omxplayer.player.OMXPlayer("/home/pi/Desktop/elephant.mp4", ['-o', 'local'])
+player = omxplayer.player.OMXPlayer(default, ['-o', 'local']) #arg local push sound to jack
+#set_video_pos(x1, y1, x2, y2)
+player.set_video_pos(200,200,627,440) #vida er 854 × 480px. Divide with two + x1 and y1
+while True:
     if GPIO.input(button1)==0: # button 1 will report 0 if it is presses
-            time.sleep(.1)
-            print ("Button Pressed")
-    player.quit()
+        time.sleep(.1)
+        print("Button Pressed")
+        player.quit()
  
     
 
 player.quit()
 GPIO.cleanup
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
-
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                x_change = -5
-            if event.key == pygame.K_RIGHT:
-                pygame.quit() #Right key close program
-                quit()
