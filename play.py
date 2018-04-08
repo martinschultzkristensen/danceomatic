a7a17734ce4a18fe46692cd8d9e3bf168f11eb6f
martinschultzkristensen <dansemartin@gmail.com>
2018-04-08 17:05:52 +0000

    added videopositioning, testet = ok

--- play.py
+++ play.py
@@ -8,17 +8,18 @@ from time import sleep
 #THE LIST SPECIFIES THE AUDIO OUTPUT TO JACK!!!
 
 
-
-player.quit()
-
 VIDEO_PATH = Path("/home/pi/Desktop/elephant.mp4")
 
-#set_video_crop(x1, y1, x2, y2)
-#cropVideo = (100,100,200,200)
-
-player = omxplayer.player.OMXPlayer(VIDEO_PATH, ['-o', 'local', '--win 100 100 200 200'])
 
 
+
+player = omxplayer.player.OMXPlayer(VIDEO_PATH, ['-o', 'local'])
+
+#set_video_pos(x1, y1, x2, y2)
+player.set_video_pos(100,100,200,200)
+print("hey")
+sleep(2)
+player.play(VIDEO_PATH)
 sleep(5)
 player.pause()
 sleep(2)
